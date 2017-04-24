b0110dbd9ecdb1ddf1b568a69a59d05169f07bcb
Carlos Filoteo <c.filoteo@live.com>
2017-04-23 22:16:06 -0600

    Add create-react-app boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1 className="title">Random User</h1>
+      </main>
+    );
+  }
+}
+
+export default App;
