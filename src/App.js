3b394a02b2345e63a90530e9745483efa3512d30
Carlos Filoteo <c.filoteo@live.com>
2017-04-23 22:55:58 -0600

    Linking button to retrieve random user's data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import fetch from 'unfetch';
 import './App.css';
 
 class App extends Component {
+  getRandomUserData() {
+    fetch('https://randomuser.me/api/')
+      .then(r => r.json())
+      .then(data => {
+        console.log(data);
+      });
+  }
   render() {
     return (
       <main>
         <div className="text-center">
-          <button className="btn" type="button">Get Random User</button>
+          <button className="btn" type="button" onClick={this.getRandomUserData}>Get Random User</button>
         </div>
         <div>
-          
+
         </div>
       </main>
     );
